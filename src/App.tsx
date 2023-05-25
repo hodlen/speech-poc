a9cab37bc0dfdfe8a5ce8d18e5f3e7d111a37d2d
Holden <34213478+hodlen@users.noreply.github.com>
2023-05-25 15:08:40 +0800

    feat: test client chat functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import reactLogo from "./assets/react.svg";
+import { AiChat } from "./components/ai-chat";
 import { SpeechRecognizorInput } from "./components/recorder-input";
 import viteLogo from "/vite.svg";
 
@@ -17,6 +18,10 @@ function App() {
       <h1>流式语音识别 POC</h1>
       <div className="card">
         <SpeechRecognizorInput />
+      </div>
+      <div>
+        Chat with ChatGPT!
+        <AiChat />
       </div>
     </>
   );
---
+++ src/components/ai-chat.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef, useState } from "react";
+import { IrisClient } from "@iris-family/client.ts";
+
+export const AiChat: React.FC = () => {
+  const clientRef = useRef(
+    new IrisClient({
+      endpoint: "ws://ec2-34-213-84-192.us-west-2.compute.amazonaws.com:8765",
+      token: "",
+    })
+  );
+  const [message, setMessage] = useState("");
+  const [thinking, setThinking] = useState("");
+  const [control, setControl] = useState("");
+  const [input, setInput] = useState("");
+  const [dialogue, setDialogue] = useState("[]");
+  useEffect(() => {
+    const client = clientRef.current;
+    client.subscribeToStreamingResponse((response) => {
+      console.log(
+        "🚀 ~ file: ai-chat.tsx:18 ~ client.subscribeToStreamingResponse ~ response:",
+        response
+      );
+      setMessage(response?.message ?? "");
+      setThinking(response?.thinking ?? "");
+      setControl(response?.controlInstructions ?? "");
+    });
+    client.subscribeToChatHistory((history) =>
+      setDialogue(JSON.stringify(history, null, 2))
+    );
+  }, []);
+
+  return (
+    <div>
+      <div>{message}</div>
+      <div>{thinking}</div>
+      <div>{control}</div>
+      <div>{dialogue}</div>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={() => clientRef.current.sendMessage(input)}>Send</button>
+    </div>
+  );
+};
